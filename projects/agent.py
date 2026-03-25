8a3f862435162ca4948bb3b305a26aa3e960fca0
Shahrukh Khan <informationteach786@gmail.com>
2026-03-25 13:34:23 +0530

    Day 19 completed - Multi-Tool Agent

--- projects/agent.py
+++ projects/agent.py
@@ -1,4 +1,5 @@
 from ai_analyzer import analyze_with_ai
+from scanner import port_scanner
 
 def security_agent(log_data):
     ai_result=analyze_with_ai(log_data)
@@ -6,15 +7,19 @@ def security_agent(log_data):
     decision="Normal Activity"
     action="No action needed"
 
+    tool_output=[]
+
     if "attack" in ai_result.lower():
         decision ="Threat Dectect"
-    if "brute" in ai_result.lower():
-        action="Recommend: Block IP / Enable rate limiting"
-    elif "mutli failed" in ai_result.lower():
-        action ="Recommend : Lock account temporaily"
+    if "brute" in ai_result.lower() or "failed login" in ai_result.lower():
+        action="Running port scan on target ...."
+        target = "127.0.0.1"
+        tool_output=port_scanner(target)
+    
 
     return{
         "analysis" : ai_result,
         "decision" : decision,
-        "action" : action
+        "action" : action,
+        "tool_output":tool_output
     }
